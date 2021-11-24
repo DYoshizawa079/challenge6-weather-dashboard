23a2f2d8f9a7db6cc6e3bd3be323269d9eeb52ec
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-11-24 14:13:52 -0500

    Began adding ability to save searches

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,27 @@
 
 var elemSearchbox = document.querySelector("#searchbox");
 var elemSubmitBtn = document.querySelector("#submit");
+var elemSearchList = document.querySelector("#searchlist");
+var searches = [];
 
 var apiKey = "0052c45ec6b9cc06b70de487e9bbc6bc";
+
+// Add past searches to localStorage and sidebar
+var addSearch = function(search) {
+    localStorage.getItem("searches");
+    console.log("searches", searches);
+    searches.push(search);
+    localStorage.setItem("searches", JSON.stringify(searches));
+    console.log("searches", searches);
+}
 
 // Get the city's name from the searchbox
 var getCity = function(event){
     event.preventDefault();
     var cityName = elemSearchbox.value;
-    console.log(cityName);
+    console.log("cityName", cityName);
     getWeatherAPI(cityName);
+    addSearch(cityName);
 }
 
 // Generate the weather API URL
