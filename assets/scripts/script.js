eccaade0319cd61e90735aae84d8edd9568b875d
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-11-24 12:36:26 -0500

    Posted first crude prototype

---
+++ assets/scripts/script.js
@@ -0,0 +1,55 @@
+
+var elemSearchbox = document.querySelector("#searchbox");
+var elemSubmitBtn = document.querySelector("#submit");
+
+var apiKey = "0052c45ec6b9cc06b70de487e9bbc6bc";
+
+// Get the city's name from the searchbox
+var getCity = function(event){
+    event.preventDefault();
+    var cityName = elemSearchbox.value;
+    console.log(cityName);
+    getWeatherAPI(cityName);
+}
+
+// Generate the weather API URL
+var getWeatherAPI = function(city) {
+
+    var latitude;
+    var longitude;
+    var geocodingAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+    var weatherAPI;
+
+    fetch(geocodingAPI)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data){
+            console.log("lat-long object",data);
+            console.log("lat", data[0].lat);
+            console.log("long", data[0].lon);
+
+            latitude = data[0].lat;
+            longitude = data[0].lon;
+
+            weatherAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=" + apiKey;
+            
+            console.log("weatherAPI",weatherAPI);
+            
+            getWeather(weatherAPI);
+        });
+}
+
+// Get the weather data fromt the generated API URL
+var getWeather = function(apiUrl) {
+    fetch(apiUrl)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log("weather object", data);
+        });
+}
+
+elemSubmitBtn.addEventListener("click",getCity);
+
